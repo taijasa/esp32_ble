8bff3e097ffe4596dbddd64fcff97cbfca94ced4
taijasa <peter.o.parapan@gmail.com>
2020-10-26 15:49:38 +0100

    Initial import

---
+++ precache-manifest.b9eddce46cb98027a29247c20588d5e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec6d82beff1e8e76c2ac060cf565484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f823d10498f05bfc82a",
+    "url": "/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "/static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f823d10498f05bfc82a",
+    "url": "/static/js/main.b7ba063a.chunk.js"
+  },
+  {
+    "revision": "dab82071668f2702c64d",
+    "url": "/static/js/runtime-main.ab6486c8.js"
+  }
+]);
